a91a036a6cc792fb60b8ca8e935c119b9da54bad
CleverChuk <chubiyke@gmail.com>
2022-03-25 23:06:38 -0400

    indexing done

--- inverted-index/indexing_algorithms.py
+++ inverted-index/indexing_algorithms.py
@@ -231,38 +231,37 @@ class Driver:
         out_size = 1048576
 
         while len(posting_filenames) > 1:
-            count = 0
             out_filename = f"out_file_{merged}.bin"
             merged += 1
 
             out_file = open(out_filename, "wb")
-            while count < out_size and len(posting_filenames) > 1:
-                filename_0, pos_0 = posting_filenames.popleft()
-                filename_1, pos_1 = posting_filenames.popleft()
 
-                file_0 = open(filename_0, "rb")
-                file_0.seek(0, os.SEEK_END)
+            filename_0, pos_0 = posting_filenames.popleft()
+            filename_1, pos_1 = posting_filenames.popleft()
 
-                size_0 = file_0.tell()
-                file_0.seek(pos_0)
+            file_0 = open(filename_0, "rb")
+            file_0.seek(0, os.SEEK_END)
 
-                file_1 = open(filename_1, "rb")
-                file_1.seek(0, os.SEEK_END)
+            size_0 = file_0.tell()
+            file_0.seek(pos_0)
 
-                size_1 = file_1.tell()
-                file_1.seek(pos_1)
-                count += algo.merge(file_0, file_1, out_file, out_size)
+            file_1 = open(filename_1, "rb")
+            file_1.seek(0, os.SEEK_END)
 
-                if file_0.tell() < size_0:
-                    posting_filenames.appendleft((filename_0, file_0.tell()))
+            size_1 = file_1.tell()
+            file_1.seek(pos_1)
+            algo.merge(file_0, file_1, out_file, out_size)
 
-                if file_1.tell() < size_1:
-                    posting_filenames.appendleft((filename_1, file_1.tell()))
+            if file_0.tell() < size_0:
+                posting_filenames.appendleft((filename_0, file_0.tell()))
 
-                file_0.close()
-                file_1.close()
+            if file_1.tell() < size_1:
+                posting_filenames.appendleft((filename_1, file_1.tell()))
 
             posting_filenames.append((out_filename, 0))
+            
+            file_0.close()
+            file_1.close()
             out_file.close()
 
 
